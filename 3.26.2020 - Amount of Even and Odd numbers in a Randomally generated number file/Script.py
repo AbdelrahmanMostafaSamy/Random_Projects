48dfa9732ab66773e9b885089818e941cf9d6dd7
Abdelrahman Mostafa <mostafasamy80@yahoo.com>
2020-03-26 01:33:58 +0200

    Creation

---
+++ 3.26.2020 - Amount of Even and Odd numbers in a Randomally generated number file/Script.py
@@ -0,0 +1,28 @@
+import logging as log
+import random
+
+with open("numberfile.txt", mode="w+") as testfile:
+    times_to_repeat = 101
+
+    while times_to_repeat > 1:
+        testfile.write("\n" + str(random.randint(1,100)))
+        times_to_repeat -= 1
+    
+    testfile.seek(0)
+    testfile = [line.replace('\n', '') for line in testfile.readlines()]
+
+amountOfEvenNumbers = 0
+amountOfOddNumbers = 0
+for line in testfile:
+    print(line)
+    try:
+        if int(line)%2 == 0:
+            amountOfEvenNumbers += 1
+            print(f"{line} is an Even Number")
+        elif int(line)%2 != 0:
+            amountOfOddNumbers += 1
+            print(f"{line} is an Odd Number")
+    except ValueError:
+        log.exception(ValueError)
+        
+print(f"\n\nAmount of Even Numbers are {amountOfEvenNumbers}, Amount of Odd Numbers are {amountOfOddNumbers}")
